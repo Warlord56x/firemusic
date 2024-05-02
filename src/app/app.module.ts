9270f99e6510ffa1f944be499d3fec9201c706d7
Warlord56x <danika200151@gmail.com>
2024-05-02 21:20:44 +0200

    Remade the dashboard, and reworked the appbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { HttpClientModule } from '@angular/common/http';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
-import { MatListItem, MatNavList, MatListModule } from '@angular/material/list';
-import { CommonModule } from '@angular/common';
-import { AngularFireModule } from '@angular/fire/compat';
-import { environment } from '../environments/environment';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MusicPlayerComponent } from './components/music-player/music-player.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { RouterOutlet } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
+import { HttpClientModule } from "@angular/common/http";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatMenuModule } from "@angular/material/menu";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatIcon, MatIconModule } from "@angular/material/icon";
+import { MatListItem, MatNavList, MatListModule } from "@angular/material/list";
+import { CommonModule } from "@angular/common";
+import { AngularFireModule } from "@angular/fire/compat";
+import { environment } from "../environments/environment";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { AngularFireStorageModule } from "@angular/fire/compat/storage";
+import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { MusicPlayerComponent } from "./components/music-player/music-player.component";
 import {
     MatFormField,
     MatPrefix,
     MatSuffix,
-} from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCard, MatCardModule } from '@angular/material/card';
+} from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCard, MatCardModule } from "@angular/material/card";
+import { MatTabsModule } from "@angular/material/tabs";
+import { UploadMusicComponent } from "./components/upload-music/upload-music.component";
+import { MatPaginator } from "@angular/material/paginator";
 
 @NgModule({
     declarations: [AppComponent, DashboardComponent],
@@ -67,6 +70,9 @@ import { MatCard, MatCardModule } from '@angular/material/card';
         MatGridListModule,
         MatCardModule,
         MatSuffix,
+        MatTabsModule,
+        UploadMusicComponent,
+        MatPaginator,
     ],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,48 +1,33 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { DatabaseService } from "../../shared/services/database.service";
 import { AuthService } from "../../shared/services/auth.service";
-import { Observable, of, Subscription } from "rxjs";
 import { Music } from "../../shared/utils/music";
-import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
-
-interface CardData {
-    music: Music;
-    cols: number;
-    rows: number;
-}
+import { PageEvent } from "@angular/material/paginator";
+import { MusicService } from "../../shared/services/music.service";
 
 @Component({
     selector: "app-dashboard",
     templateUrl: "./dashboard.component.html",
     styleUrl: "./dashboard.component.scss",
 })
-export class DashboardComponent implements OnDestroy, OnInit {
-    cards$: Observable<CardData[]> = new Observable();
-    musics: CardData[] | undefined;
-
-    breakSubscription: Subscription | undefined;
+export class DashboardComponent implements OnInit {
+    musics: Music[] | undefined;
+    protected pageSize: number = 10;
+    protected pageIndex: number = 0;
 
     constructor(
         private databaseService: DatabaseService,
         private authService: AuthService,
-        private breakpointObserver: BreakpointObserver,
+        private musicService: MusicService,
     ) {}
+
+    play(m: Music) {
+        this.musicService.changeMusic(m);
+    }
 
     ngOnInit(): void {
         this.getCards().then((res) => {
-            const cardData = res.map((music) => {
-                return { music: music, cols: 1, rows: 1 };
-            });
-            this.cards$ = of(cardData);
-            this.musics = cardData;
-
-            this.breakSubscription = this.breakpointObserver
-                .observe(Breakpoints.Handset)
-                .subscribe(({ matches }) => {
-                    this.musics?.forEach(
-                        (musicData) => (musicData.cols = matches ? 2 : 1),
-                    );
-                });
+            this.musics = res;
         });
     }
 
@@ -52,7 +37,13 @@ export class DashboardComponent implements OnDestroy, OnInit {
         );
     }
 
-    ngOnDestroy(): void {
-        this.breakSubscription?.unsubscribe();
+    pageChange(event: PageEvent) {
+        console.log(event);
+        this.pageSize = event.pageSize;
+        this.pageIndex = event.pageIndex;
+        console.log(
+            this.pageIndex * this.pageSize,
+            (this.pageIndex + 1) * this.pageSize,
+        );
     }
 }
